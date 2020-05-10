fa99dae80811324b54a80d455f88b780865ff260
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-05-10 13:49:12 +0100

    styling practice

--- App.js
+++ App.js
@@ -1,19 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>
+          Here are some boxes of different colors
+        </Text>
+        <View style={[styles.button, styles.cyan]}>
+          <Text style={styles.text}>Cyan #2aa198</Text>
+        </View>
+        <View style={[styles.button, styles.blue]}>
+          <Text style={styles.text}>Blue #268bd2</Text>
+        </View>
+        <View style={[styles.button, styles.magenta]}>
+          <Text style={styles.text}>Magenta #d33682</Text>
+        </View>
+        <View style={[styles.button, styles.orange]}>
+          <Text style={styles.text}>Orange #cb4b16</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    marginVertical: 32,
+    marginHorizontal: 16,
+  },
+  heading: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    paddingBottom: 16,
+  },
+  button: {
+    color: 'white',
+    borderRadius: 3,
+    marginBottom: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 8,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
 });
+
+export default App;
