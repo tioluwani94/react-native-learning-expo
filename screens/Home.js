af25c48f4d7cb0f299a99d96a37774e5cb629137
Tioluwani Kolawole <kolawole8tiolu@gmail.com>
2020-05-10 22:18:13 +0100

    complete network request exercise

--- screens/Home.js
+++ screens/Home.js
@@ -3,48 +3,9 @@ import { StyleSheet, View } from 'react-native';
 import ColorsPreview from '../components/ColorsPreview';
 import { FlatList } from 'react-native-gesture-handler';
 
-const SOLARIZED = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
-
-const RAINBOW = [
-  { colorName: 'Red', hexCode: '#FF0000' },
-  { colorName: 'Orange', hexCode: '#FF7F00' },
-  { colorName: 'Yellow', hexCode: '#FFFF00' },
-  { colorName: 'Green', hexCode: '#00FF00' },
-  { colorName: 'Violet', hexCode: '#8B00FF' },
-];
-
-const FRONTEND_MASTERS = [
-  { colorName: 'Red', hexCode: '#c02d28' },
-  { colorName: 'Black', hexCode: '#3e3e3e' },
-  { colorName: 'Grey', hexCode: '#8a8a8a' },
-  { colorName: 'White', hexCode: '#ffffff' },
-  { colorName: 'Orange', hexCode: '#e66225' },
-];
-
-const COLOR_PALETTES = [
-  { colors: SOLARIZED, paletteName: 'Solarized' },
-  { colors: FRONTEND_MASTERS, paletteName: 'Frontend Masters' },
-  { colors: RAINBOW, paletteName: 'Rainbow' },
-];
-
 const Home = ({ navigation }) => {
+  const [colorPalettes, setColorPalettes] = React.useState([]);
+
   const handlePress = ({ colors, paletteName }) => {
     navigation.navigate('ColorPalette', {
       colors,
@@ -52,10 +13,34 @@ const Home = ({ navigation }) => {
     });
   };
 
+  const fetchColorPalettes = React.useCallback(async () => {
+    try {
+      const response = await fetch(
+        ' https://color-palette-api.kadikraman.now.sh/palettes',
+        {
+          headers: {
+            Accept: 'application/json',
+          },
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setColorPalettes(data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    fetchColorPalettes();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
-        data={COLOR_PALETTES}
+        data={colorPalettes}
         keyExtractor={(item) => item.paletteName}
         renderItem={({ item }) => (
           <ColorsPreview palette={item} handlePress={handlePress} />
